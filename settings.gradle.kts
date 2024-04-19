b14f138fea65090a899819f972f1354362f2986b
Kristaps Murnieks <118051283+KMurnieks@users.noreply.github.com>
2024-04-19 12:50:28 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kristaps_Murnieks_221RDB173_Krustini_Nullites"
+include(":app")
